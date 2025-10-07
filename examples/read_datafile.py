7b271d82a654f0b5eac37e43df849141126389ad
Jurriaan Schreuder <jurschreuder@gmail.com>
2025-10-07 19:33:24 +0200

    added first example

---
+++ examples/read_datafile.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from kexxu_eye_sdk.format import openeye_file_to_xy
+
+path = "examples/example-openeye-datafile.json"
+eye_locs = openeye_file_to_xy(path)
+
+print("example output:")
+print()
+for i in range(20):
+    print(eye_locs[i])
+print()
+print("number of eye locations loaded:", len(eye_locs))
+
+
