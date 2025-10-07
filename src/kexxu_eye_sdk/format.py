630a053676f32c8c331d6eb89fa61594b47ffc04
Jurriaan Schreuder <jurschreuder@gmail.com>
2025-10-07 19:13:06 +0200

    added first real tool

--- src/kexxu_eye_sdk/format.py
+++ src/kexxu_eye_sdk/format.py
@@ -1,12 +1,59 @@
 # Written by: Jurriaan Schreuder
 
-# Test unit test setup
-def add(a: int, b: int):
-    return a + b
+import json
 
 def openeye_file_to_xy(path: str):
-  print("todo")
+  eye_locs = []
+  lines = read_lines(path)
+  for line in lines:
+    try:
+      if not get_event_type(line) == "eyetracking":
+          continue # not an eye tracking event, skip
+
+      parts = get_event_parts(line)
+      js = json.loads(parts[2])
+
+      rx = js["pupil_rel_pos_x"]
+      ry = js["pupil_rel_pos_y"]
+
+      x, y = to_720p(rx, ry)
+      eye_locs.append([x, y])
+    except Exception as e:
+        print("error processing event from line:")
+        print(line)
+   
+  return eye_locs
+  
 
 def xy_file_to_openeye(arr: list):
   print("todo")
+  return True
 
+
+def read_lines(path: str):
+  with open(path, "r") as f:
+    lines = [line.strip() for line in f]
+  return lines
+
+def save_lines(lines: list, path: str):
+  with open(path, "w") as f:
+    f.write("\n".join(lines))
+
+def get_event_type(line):
+  p0 = get_event_parts(line)[0]
+  return p0.rsplit("/", 1)[-1]
+
+def get_event_parts(line):
+  # event type, timestamp, json
+  parts = line.strip().split(" ", 2)
+  return parts
+
+def to_720p(rx, ry):
+  x = 640 - int((rx) * 640.0)
+  y = 360 + int((ry) * 360.0)
+  return x, y
+
+def from_720p(x, y):
+  rx = (640 - x) / 640.0 
+  ry = (y - 360) / 360.0
+  return rx, ry
--- tests/test_format.py
+++ tests/test_format.py
@@ -1,7 +1,19 @@
 # Written by: Jurriaan Schreuder
 
-from kexxu_eye_sdk.format import add
+from pathlib import Path
 
-# Test unit test setup
-def test_add():
-    assert add(1, 1) == 2
+from kexxu_eye_sdk.format import openeye_file_to_xy
+from kexxu_eye_sdk.format import xy_file_to_openeye
+
+# Test reading in an openeye file and getting the eye x,y coords
+def test_openeye_file_to_xy():
+    data_dir = Path(__file__).parent / "data"
+    test_file_path = data_dir / "example-openeye-datafile.json"
+    eye_locs = openeye_file_to_xy(test_file_path)
+    assert len(eye_locs) == 691
+
+
+def test_xy_to_openeye():
+    # TODO
+    abc = xy_file_to_openeye([])
+    assert abc
